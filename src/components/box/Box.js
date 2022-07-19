5c47cc90483347f0520a5369ba4913931dc89d55
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-07-19 15:26:34 +0300

    edit

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-  margin: 0 auto;
+  margin: 20px auto 0;
   align-items: center;
   width: 380px;
-  background-color: aqua;
   border-radius: ${({ theme }) => theme.radii.normal};
 `;
